adce03dda617616834f5a5f208d51150bb4b6655
Lucian Gruia <luciancgruia@gmail.com>
2021-02-25 21:57:08 +0200

    initial commit

---
+++ src/main/java/com/search/wildcardsearch/WildcardSearchApplication.java
@@ -0,0 +1,13 @@
+package com.search.wildcardsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcardSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcardSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/search/wildcardsearch/WildcardSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.search.wildcardsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcardSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
